3a82b311180df411131a590934b9afe10b806fe0
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2026-01-21 15:56:52 -0300

    wip: layed rough interface

---
+++ packages/privacy-pools/src/types/base.ts
@@ -0,0 +1,5 @@
+export type U256 = bigint;
+export type AssetId = string;
+export type Address = string;
+export type AccountId = string;
+export type Bytes = Uint8Array;
---
+++ packages/privacy-pools/src/types/index.ts
@@ -0,0 +1,61 @@
+import { AccountId, Address, AssetId, U256 } from "./base";
+import { HostInterface } from "./host";
+
+type Transaction = unknown;
+
+export interface PrepareShieldResult {
+  transactions: Array<Transaction>;
+  // TODO: Additional metadata as needed
+}
+
+// Represents a prepared operation that can be submitted to the privacy protocol.
+export interface PoolOperation {
+  // Inner is data used by the pool to submit the operation. This is explicitly unknown the SDK consumer, is unstable, and will vary from pool to pool.
+  inner: undefined;
+  // TODO: Additional metadata as needed
+}
+
+export interface PrivacyProtocolConstructor {
+  new(host: HostInterface): PrivacyProtocol;
+}
+
+export interface PrivacyProtocol {
+
+  // Prepares a shield operation.
+  //
+  // Returns the list of transactions that will be broadcasted to perform the shield.
+  //
+  // Returns an error if the shield cannot be performed.
+  prepareShield(assets: Array<{ asset: AssetId, amount: U256; }>): PrepareShieldResult;
+
+  // Prepares an unshield operation.
+  // 
+  // TODO: Add structured metadata if needed for client-side validation.
+  //
+  // Returns an error if the unshield cannot be performed.
+  prepareUnshield(
+    target: Address,
+    assets: Array<{ asset: AssetId, amount: U256; }>,
+  ): PoolOperation;
+
+  // Prepares a transfer operation.
+  // 
+  // TODO: Add structured metadata if needed for client-side validation.
+  //
+  // Returns an error if the transfer cannot be performed.
+  prepareTransfer(
+    target: AccountId,
+    assets: Array<{ asset: AssetId, amount: U256; }>,
+  ): PoolOperation;
+
+  // Broadcasts a PoolOperation constructed by this plugin.
+  // 
+  // Returns an error if the operation was not constructed by this plugin or cannot otherwise be broadcasted.
+  broadcast(operation: PoolOperation): void;
+
+  // Returns the current balance of the specified asset held by this pool
+  // 
+  // If assets is empty, returns the balances for all assets.
+  balance(assets: Array<{ asset: AssetId; }>): Array<{ asset: AssetId; balance: U256; }>;
+
+}
