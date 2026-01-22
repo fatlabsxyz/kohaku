a6f9b822e2c1f28d2ecb877c7961ee2f42dcf913
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2026-01-22 12:43:49 -0300

    wip: secret abstraction

--- packages/privacy-pools/src/account/keys.ts
+++ packages/privacy-pools/src/account/keys.ts
@@ -1,32 +1,16 @@
 import { HDNodeWallet, Wallet, keccak256, concat, toUtf8Bytes, getBytes } from 'ethers';
+import { Bytes } from '../types/base';
+import { poseidon } from "maci-crypto/build/ts/hashing.js";
 
 /** BIP32-BIP43 - Privacy Pools v1
  *                                                LABEL >>>
- * m/purpose'/version'/account'/ -- /secret_type'/deposit'   /withdraw
+ * m/purpose'/version'/chain'/account'/ -- /secret_type'/deposit'   /withdraw
  *                                                  L-> PH[depositSecret(N|C), entrypointAddress] -> circuit
  *                                                              L-> PH[withdrawSecret(N|C), entrypointAddress] -> circuit
  */
-// m/28784'/1'/3'/0'/n' | -> precomm
-// m/28784'/1'/3'/1'/n' |
-
-// m/28784'/1'/3'/0'/6' < 10 usd
-// m/28784'/1'/3'/0'/6'/0 < -5 usd
-//
-// m/28784'/1'/3'/0'/7' < 5 usd
-//
-// deposit (+1)
-// m/28784'/1'/3'/0'/6'+1/4 <
-//
-// m/28784'/1'/3'/0'/DEPOSIT === 0xCaFe ;
-// m/28784'/1'/3'/1'/DEPOSIT === 0xAbEd ;
-// Nodo(0xCaFe).derive(1)
-// Nodo(0xAbEd).derive(1)
-// precommitment <- PH[commitment|nullifier]
-//
-// label <- PH[scope, nonce]
 
 const PRIVACY_POOLS_PATH = "m/28784'/1'";
-const HARDENED_OFFSET = 0x80000000
+const HARDENED_OFFSET = 0x80000000;
 
 export type KeyConfig =
   | { type: 'mnemonic'; mnemonic: string; accountIndex: number; }
@@ -44,8 +28,8 @@ export const deriveKeysFromMnemonic = (mnemonic: string, accountIndex: number): 
 
   const derivedWallet = wallet.deriveChild(accountIndex + HARDENED_OFFSET);
 
-  const derivedMasterCommitment = derivedWallet.deriveChild(0 + HARDENED_OFFSET)
-  const derivedMasterNullifier = derivedWallet.deriveChild(1 + HARDENED_OFFSET)
+  const derivedMasterCommitment = derivedWallet.deriveChild(0 + HARDENED_OFFSET);
+  const derivedMasterNullifier = derivedWallet.deriveChild(1 + HARDENED_OFFSET);
 
   const commitmentKey = getBytes(derivedMasterCommitment.privateKey);
   const nullifierKey = getBytes(derivedMasterNullifier.privateKey);
@@ -66,3 +50,83 @@ export const deriveKeys = (config: KeyConfig): DerivedKeys => {
 
   return deriveKeysFromPrivateKeys(config.commitmentKey, config.nullifierKey);
 };
+
+function hashToSnarkField(numberLikes: (string | bigint)[]) {
+  const _bigints: bigint[] = [];
+  for (const n of numberLikes) {
+    if (typeof n === 'string') {
+      try {
+        _bigints.push(BigInt(n));
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      _bigints.push(n);
+    }
+  }
+  return poseidon(_bigints);
+}
+
+const derHard = (w: HDNodeWallet, index: number) => w.deriveChild(index + HARDENED_OFFSET);
+
+type Secret = {
+  nullifier: bigint;
+  commitment: bigint;
+  precommitment: bigint;
+};
+
+interface ISecretManager {
+  getDepositSecrets: (index: number) => Secret;
+  getWithdrawalSecrets: (depositIndex: number, index: number) => Secret;
+}
+
+
+type PrivacyPoolsDerivationPath = {
+  chain: number;
+  account: number;
+  secretType: "salt" | "nullifier";
+  deposit: number;
+  withdraw?: number;
+};
+
+const ppPath = ({ chain, account, secretType, deposit, withdraw }: PrivacyPoolsDerivationPath) => {
+  const _secretType = secretType === "nullifier" ? 0 : 1;
+  const depositPath = `${PRIVACY_POOLS_PATH}/${chain}'/${account}'/${_secretType}'/${deposit}'`;
+  if (withdraw) {
+    return `${depositPath}/${withdraw}'`;
+  } else {
+    return depositPath;
+  }
+};
+
+export function SecretManager(protocolSeed: Bytes, accountIndex: number = 0): ISecretManager {
+
+  const wallet = HDNodeWallet.fromSeed(protocolSeed);
+
+  const accountWallet = wallet.deriveChild(accountIndex + HARDENED_OFFSET);
+
+  const accountMasterCommitment = accountWallet.deriveChild(0 + HARDENED_OFFSET);
+  const accountMasterNullifier = accountWallet.deriveChild(1 + HARDENED_OFFSET);
+
+  const deriveSecrets = (nullifierNode: HDNodeWallet, commitmentNode: HDNodeWallet, index: number) => {
+    const nullifier = hashToSnarkField([derHard(nullifierNode, index).privateKey]);
+    const commitment = hashToSnarkField([derHard(commitmentNode, index).privateKey]);
+    const precommitment = hashToSnarkField([nullifier, commitment]);
+    return { nullifier, commitment, precommitment };
+  };
+
+  const getDepositSecrets = (index: number) => {
+    return deriveSecrets(accountMasterNullifier, accountMasterCommitment, index);
+  };
+
+  const getWithdrawalSecrets = (depositIndex: number, index: number) => {
+    const depositNullifier = derHard(accountMasterNullifier, depositIndex);
+    const depositCommitment = derHard(accountMasterCommitment, depositIndex);
+    return deriveSecrets(depositNullifier, depositCommitment, index);
+  };
+
+  return {
+    getDepositSecrets,
+    getWithdrawalSecrets
+  };
+}
--- packages/privacy-pools/src/account/tx/shield.ts
+++ packages/privacy-pools/src/account/tx/shield.ts
@@ -1,9 +1,11 @@
+import { HostInterface } from '../../types/host';
 import { Address } from 'viem';
 import { Interface } from 'ethers';
 import { createTx, TxData } from '@kohaku-eth/provider';
 import { PPv1NetworkConfig } from '../../config';
 import { CommitmentActions } from '../actions/commitment';
 import { Commitment } from '../types';
+import { AssetId, U256 } from '../../types/base';
 
 export type ShieldFn = (token: Address, value: bigint) => { commitment: Commitment; tx: TxData; };
 export type Shield = { shield: ShieldFn; };
@@ -62,4 +64,42 @@ export const makeCreateShield = async ({ network, actions }: CreateShieldContext
 
 function isNative(token: string) {
   return token.toLowerCase() === "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+}
+
+type PrepareShieldContext = {
+  host: HostInterface;
+  shield: { asset: AssetId; amount: U256; };
+};
+
+export function prepareNativeShield(host: HostInterface, amount: bigint) {
+    // 2. Encode transaction
+    const data = ENTRYPOINT_INTERFACE_NATIVE.encodeFunctionData('deposit', [commitment.hash]);
+    const tx = createTx(network.ENTRYPOINT_ADDRESS, data, value);
+
+    // Return both commitment and tx
+    // User should: 1) submit tx, 2) wait for confirmation, 3) call addCommitment()
+    return { commitment, tx };
+}
+
+export function prepareErc20Shield(host: HostInterface, asset: string, amount: bigint) {
+    
+    // 1. Generate commitment
+    const commitment = host.keystore.createCommitment(token, value);
+
+    // 2. Encode transaction
+    const data = ENTRYPOINT_INTERFACE_ERC20.encodeFunctionData('deposit', [token, value, commitment.hash]);
+    const tx = createTx(network.ENTRYPOINT_ADDRESS, data);
+
+    // Return both commitment and tx
+    // User should: 1) submit tx, 2) wait for confirmation, 3) call addCommitment()
+    return { commitment, tx };
+}
+
+export async function prepareShield({ host, shield }: PrepareShieldContext) {
+  const { asset, amount } = shield;
+  if (isNative(asset)) {
+    return prepareNativeShield(host, amount);
+  } else {
+    return prepareErc20Shield(host, asset, amount);
+  }
 }
