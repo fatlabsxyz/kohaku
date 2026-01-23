b8411e8bc6d3e64fb4f72cc1f8a356c0defaac93
Gomorongi <elmascapito@fatsolutions.xyz>
2026-01-23 18:29:13 +0100

    event fetching done

---
+++ packages/privacy-pools/src/data/data.service.ts
@@ -0,0 +1,33 @@
+import { EthereumProvider } from "@kohaku-eth/provider";
+import { GetEventsFn, IDataService } from "./interfaces/data.service.interface";
+import { parseEventLogs } from "viem";
+import { EVENTS_SIGNATURES, EventTypes } from "./abis/events.abi";
+import { EVENTS_PARSERS } from "./utils/events-parsers.util";
+
+export interface DataServiceParams {
+    provider: EthereumProvider
+}
+
+export class DataService implements IDataService {
+    private readonly provider!: EthereumProvider;
+
+    constructor(params: DataServiceParams) {
+        Object.assign(this, params);
+    }
+
+    getEvents: GetEventsFn = async ({events, ...params}) => {
+        const logs = await this.provider.getLogs({
+            ...params
+        });
+        const allEvents = events instanceof Array ? events : [events];
+        return allEvents.reduce((parsedEvents, eventType) => ({
+            ...parsedEvents,
+            [eventType]: parseEventLogs({
+                logs: logs as never,
+                abi: [EVENTS_SIGNATURES[eventType]] as const,
+                eventName: eventType as never,
+                strict: true
+            } as const).map((parsedLog) => EVENTS_PARSERS[eventType](parsedLog as never))
+        }), {} as Awaited<ReturnType<GetEventsFn>>);
+    }
+}
