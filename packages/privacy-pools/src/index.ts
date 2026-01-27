921c749dfdb362d1947da309173c866d0ef2f636
Gomorongi <elmascapito@fatsolutions.xyz>
2026-01-27 13:46:08 -0300

    Fix build

--- packages/privacy-pools/src/index.ts
+++ packages/privacy-pools/src/index.ts
@@ -5,8 +5,7 @@ export { PrivacyPoolsV1Protocol } from './plugin/base';
 export type { SecretManager, SecretManagerParams, ISecretManager } from './account/keys';
 export type { Commitment, Nullifier } from './account/types';
 export type { PPv1NetworkConfig as NetworkConfig } from './config';
-export type { AssetId, ChainId, Address, U256 } from './types/base';
-export type { Transaction, PrepareShieldResult } from './types';
+export type { EvmChainId } from './types/base';
 
 // Configs
 export { MOCK_POOL_ADDRESS, MOCK_VERIFIER_ADDRESS, SEPOLIA_CONFIG, MAINNET_CONFIG, E_ADDRESS } from './config';
