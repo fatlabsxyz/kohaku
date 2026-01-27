af04cdd81244680a55054d53c92b15bfed51bfac
Gomorongi <elmascapito@fatsolutions.xyz>
2026-01-27 13:46:08 -0300

    Fix build

--- packages/privacy-pools/src/types/base.ts
+++ packages/privacy-pools/src/types/base.ts
@@ -1,27 +1,3 @@
-export type Option = 
-  | { kind: "text"; label: string; }
-  | { kind: "select"; label: string; options: string[]; };
+import { ChainId } from "@kohaku-eth/plugins";
 
-// caip-style Account & Asset IDs.
-export type Address = string;
-export type U256 = bigint;
-export type Bytes = Uint8Array;
-
-export type ChainId =
-  | { kind: "Evm"; chainId: bigint; }
-  | { kind: "Custom"; namespace: string; reference: string; };
-
-export interface AssetId {
-  chainId: ChainId;
-  assetType: AssetType;
-}
-
-export interface AccountId {
-  chainId: ChainId;
-  accountId: string;
-}
-
-export type AssetType =
-  | { kind: "Slip44"; coinType: bigint }
-  | { kind: "Erc20"; address: Address }
-  | { kind: "Erc721"; address: Address };
+export type EvmChainId = Exclude<ChainId, { kind: "Custom" }>;
