e4b5d1131dddc44a28b7992b6aa5a6585c309350
Gomorongi <elmascapito@fatsolutions.xyz>
2026-01-28 14:32:33 -0300

    Added entrypointDeposits storage

--- packages/privacy-pools/src/state/store.ts
+++ packages/privacy-pools/src/state/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import depositsReducer from './slices/depositsSlice';
+import entrypointDepositsReducer from './slices/entrypointDepositsSlice';
 import withdrawalsReducer from './slices/withdrawalsSlice';
 import ragequitsReducer from './slices/ragequitsSlice';
 
 export const storeFactory = () => configureStore({
   reducer: {
     deposits: depositsReducer,
+    entrypointDeposits: entrypointDepositsReducer,
     withdrawals: withdrawalsReducer,
     ragequits: ragequitsReducer,
   },
