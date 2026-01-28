1624eb8145108d6c04ec58a5df57c03a699c2f71
Gomorongi <elmascapito@fatsolutions.xyz>
2026-01-28 14:50:36 -0300

    Forgotten types

--- packages/privacy-pools/src/types/base.ts
+++ packages/privacy-pools/src/types/base.ts
@@ -1,3 +1,5 @@
-import { ChainId } from "@kohaku-eth/plugins";
+import { AssetId, ChainId } from "@kohaku-eth/plugins";
 
 export type EvmChainId = Exclude<ChainId, { kind: "Custom" }>;
+
+export type EvmAssetId = AssetId & { chainId: { kind: "Evm" } };
