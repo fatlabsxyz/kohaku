99bdf99868852303f20b04db6d74c35b8abee8e3
Gomorongi <elmascapito@fatsolutions.xyz>
2026-01-29 12:22:52 -0300

    Adapt to new ChainId interfaces

--- packages/privacy-pools/src/index.ts
+++ packages/privacy-pools/src/index.ts
@@ -5,7 +5,6 @@ export { PrivacyPoolsV1Protocol } from './plugin/base';
 export type { SecretManager, SecretManagerParams, ISecretManager } from './account/keys';
 export type { Commitment, Nullifier } from './account/types';
 export type { PPv1NetworkConfig as NetworkConfig } from './config';
-export type { EvmChainId } from './types/base';
 
 // Configs
 export { MOCK_POOL_ADDRESS, MOCK_VERIFIER_ADDRESS, SEPOLIA_CONFIG, MAINNET_CONFIG, E_ADDRESS } from './config';
