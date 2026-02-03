3afa933eab6f20991d2a2bc47cc95f5ad04f71d3
Gomorongi <elmascapito@fatsolutions.xyz>
2026-02-03 15:21:09 -0300

    Added relayer client and removed ethers

--- packages/privacy-pools/src/data/abis/entrypoint.abi.ts
+++ packages/privacy-pools/src/data/abis/entrypoint.abi.ts
@@ -1,107 +1,149 @@
+import { Abi } from "viem";
+
+export const entrypointDepositErc20Abi = [
+  {
+    type: "function",
+    name: "deposit",
+    inputs: [
+      {
+        name: "_asset",
+        type: "address",
+        internalType: "contract IERC20",
+      },
+      {
+        name: "_value",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "_precommitment",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        name: "_commitment",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+] as const satisfies Abi;
+
+export const entrypointDepositNativeAbi = [
+  {
+    type: "function",
+    name: "deposit",
+    inputs: [
+      {
+        name: "_precommitment",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        name: "_commitment",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "payable",
+  },
+] as const satisfies Abi;
+
 export const entrypointAbi = [
   {
-    "type": "function",
-    "name": "deposit",
-    "inputs": [
+    type: "function",
+    name: "relay",
+    inputs: [
       {
-        "name": "_asset",
-        "type": "address",
-        "internalType": "contract IERC20"
+        name: "_withdrawal",
+        type: "tuple",
+        internalType: "struct IPrivacyPool.Withdrawal",
+        components: [
+          {
+            name: "processooor",
+            type: "address",
+            internalType: "address",
+          },
+          {
+            name: "data",
+            type: "bytes",
+            internalType: "bytes",
+          },
+        ],
       },
       {
-        "name": "_value",
-        "type": "uint256",
-        "internalType": "uint256"
+        name: "_proof",
+        type: "tuple",
+        internalType: "struct ProofLib.WithdrawProof",
+        components: [
+          {
+            name: "pA",
+            type: "uint256[2]",
+            internalType: "uint256[2]",
+          },
+          {
+            name: "pB",
+            type: "uint256[2][2]",
+            internalType: "uint256[2][2]",
+          },
+          {
+            name: "pC",
+            type: "uint256[2]",
+            internalType: "uint256[2]",
+          },
+          {
+            name: "pubSignals",
+            type: "uint256[8]",
+            internalType: "uint256[8]",
+          },
+        ],
       },
       {
-        "name": "_precommitment",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "_scope",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "outputs": [
-      {
-        "name": "_commitment",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
-    ],
-    "stateMutability": "nonpayable"
+    outputs: [],
+    stateMutability: "nonpayable",
   },
   {
-    "type": "function",
-    "name": "deposit",
-    "inputs": [
+    type: "function",
+    name: "assetConfig",
+    inputs: [
       {
-        "name": "_precommitment",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "_asset",
+        type: "address",
+        internalType: "contract IERC20",
+      },
     ],
-    "outputs": [
+    outputs: [
       {
-        "name": "_commitment",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "pool",
+        type: "address",
+        internalType: "contract IPrivacyPool",
+      },
+      {
+        name: "minimumDepositAmount",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "vettingFeeBPS",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "maxRelayFeeBPS",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "stateMutability": "payable"
+    stateMutability: "view",
   },
-  {
-    "type": "function",
-    "name": "relay",
-    "inputs": [
-      {
-        "name": "_withdrawal",
-        "type": "tuple",
-        "internalType": "struct IPrivacyPool.Withdrawal",
-        "components": [
-          {
-            "name": "processooor",
-            "type": "address",
-            "internalType": "address"
-          },
-          {
-            "name": "data",
-            "type": "bytes",
-            "internalType": "bytes"
-          }
-        ]
-      },
-      {
-        "name": "_proof",
-        "type": "tuple",
-        "internalType": "struct ProofLib.WithdrawProof",
-        "components": [
-          {
-            "name": "pA",
-            "type": "uint256[2]",
-            "internalType": "uint256[2]"
-          },
-          {
-            "name": "pB",
-            "type": "uint256[2][2]",
-            "internalType": "uint256[2][2]"
-          },
-          {
-            "name": "pC",
-            "type": "uint256[2]",
-            "internalType": "uint256[2]"
-          },
-          {
-            "name": "pubSignals",
-            "type": "uint256[8]",
-            "internalType": "uint256[8]"
-          }
-        ]
-      },
-      {
-        "name": "_scope",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  }
-] as const;
+] as const satisfies Abi;
