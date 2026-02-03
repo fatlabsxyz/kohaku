13bdaedf56d130cd95624a369feb8c9821f76578
Gomorongi <elmascapito@fatsolutions.xyz>
2026-02-03 14:18:38 -0300

    Fix pool events sync and added utility selectors

--- packages/privacy-pools/src/state/selectors/pools.selector.ts
+++ packages/privacy-pools/src/state/selectors/pools.selector.ts
@@ -1,8 +1,11 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { IPool } from "../../data/interfaces/events.interface";
+import { IPool, IPoolDepositEvent, IRagequitEvent, IWithdrawalEvent } from "../../data/interfaces/events.interface";
 import { RootState } from "../store";
-import { createMyEntrypointDepositsSelector } from "./deposits.selector";
+import { createMyEntrypointDepositsSelector, depositsSelector } from "./deposits.selector";
 import { selectEntityMap } from "../utils/selectors.utils";
+import { withdrawalsSelector } from "./withdrawals.selector";
+import { ragequitsSelector } from "./ragequits.selector";
+import { Address, Commitment, Nullifier, Precommitment } from "../../interfaces/types.interface";
 
 export const poolsSelector = selectEntityMap((s) => s.pools.poolsTuples);
 
@@ -25,23 +28,55 @@ export const createMyPoolsSelector = (
   );
 };
 
-export const createMyUnsyncedPoolsAddresses = (
-  myEntrypointDepositsSelector: ReturnType<typeof createMyEntrypointDepositsSelector>
-) => {
-  return createSelector(
-    [
-      myEntrypointDepositsSelector,
-      poolsSelector,
-    ],
-    (myEntrypointDeposits, pools): bigint[] => {
-      const uniquePoolAddresses = new Set(
-        Array.from(myEntrypointDeposits.values())
-          .map(({ poolAddress }) => poolAddress)
-      );
+export const poolEventsSelector = createSelector(
+  [
+    depositsSelector,
+    withdrawalsSelector,
+    ragequitsSelector,
+    (state: RootState, poolAddress: Address) => poolAddress,
+  ],
+  (deposits, withdrawals, ragequits, poolAddress) => {
+    // Filter deposits by pool address
+    const filteredDeposits = new Map(
+      Array.from(deposits)
+        .filter(([_, deposit]) => deposit.pool === poolAddress)
+    );
 
-      return Array.from(uniquePoolAddresses).filter(
-        (address) => !pools.has(address)
-      );
-    }
-  );
-};
+    // Filter withdrawals by pool address
+    const filteredWithdrawals = new Map(
+      Array.from(withdrawals)
+        .filter(([_, withdrawal]) => withdrawal.pool === poolAddress)
+    );
+
+    // Filter ragequits by pool address
+    const filteredRagequits = new Map(
+      Array.from(ragequits)
+        .filter(([_, ragequit]) => ragequit.pool === poolAddress)
+    );
+
+    return {
+      deposits: filteredDeposits,
+      withdrawals: filteredWithdrawals,
+      ragequits: filteredRagequits,
+    };
+  }
+);
+
+export const poolCommitmentsSelector = createSelector(
+  [poolEventsSelector],
+  (poolEvents): Set<Commitment> => {
+    const { deposits, withdrawals } = poolEvents;
+
+    const depositsArray = Array.from(deposits.values());
+    const withdrawalsArray = Array.from(withdrawals.values());
+    const combined = [...depositsArray, ...withdrawalsArray];
+
+    const eventsSorterByBlockNumber = combined.sort((a, b) => {
+      return Number(a.blockNumber - b.blockNumber);
+    });
+
+    return new Set(
+      eventsSorterByBlockNumber.map(({commitment}) => commitment)
+    );
+  }
+);
