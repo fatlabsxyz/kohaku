7922dad2b21bb37ae59370789672c2f41986d665
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2026-02-04 13:50:52 -0300

    fix: build issues

--- packages/privacy-pools/src/plugin/interfaces/protocol-params.interface.ts
+++ packages/privacy-pools/src/plugin/interfaces/protocol-params.interface.ts
@@ -3,8 +3,8 @@ import { Eip155ChainId } from '@kohaku-eth/plugins';
 import { TxData } from '@kohaku-eth/provider';
 import { ISecretManager, SecretManagerParams } from "../../account/keys";
 import { Address } from "../../interfaces/types.interface";
-import { StoreFactoryParams } from "../../state/state-manager";
 import { IQuoteResponse, IRelayerClient, WithdrawalPayload } from '../../relayer/interfaces/relayer-client.interface';
+import { StoreFactoryParams } from "../../state/state-manager";
 
 type ProveOutput = Awaited<ReturnType<Awaited<ReturnType<typeof Prover>>['prove']>>;
 
