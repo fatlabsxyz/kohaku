7d5c4ad865743b09c5126027d165208f1b12648a
Gomorongi <elmascapito@fatsolutions.xyz>
2026-02-06 15:30:13 -0300

    Improve serialization and fix selectors running needlessly

--- packages/privacy-pools/src/state/utils/selectors.utils.ts
+++ packages/privacy-pools/src/state/utils/selectors.utils.ts
@@ -1,11 +1,17 @@
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector, Tuple } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import { Deserialize } from "../interfaces/utils.interface";
+import { deserialize } from "./serialize.utils";
 
 export const selectEntityMap = <
-    const SelectorType extends (state: RootState) => [any, any][]
->(mapFn: SelectorType) =>
-    createSelector(
-    [
-        mapFn,
-    ], 
-    (tuple) => new Map(tuple)) as unknown as (state: RootState) => SelectorType extends (...params: any) => [infer Key, infer Value][] ? Map<Key, Value> : never;
+  const SelectorType extends (state: RootState) => [any, any][],
+  const TransformFn extends (
+    tuple: ReturnType<SelectorType>[number],
+  ) => [Deserialize<(typeof tuple)[0]>, Deserialize<(typeof tuple)[1]>],
+>(
+  mapFn: SelectorType,
+  transform: TransformFn = deserialize as TransformFn,
+): ((
+  state: RootState,
+) => Map<ReturnType<TransformFn>[0], ReturnType<TransformFn>[1]>) =>
+  createSelector([mapFn], (tuple) => new Map(tuple.map(transform))) as never;
--- packages/privacy-pools/src/state/utils/serialize.utils.ts
+++ packages/privacy-pools/src/state/utils/serialize.utils.ts
@@ -1,4 +1,8 @@
-import { Deserialize, Serializable } from "../interfaces/utils.interface";
+import {
+  DeserializeDictionary,
+  DeserializeWithDictionary,
+  Serializable,
+} from "../interfaces/utils.interface";
 
 export const serialize = <const T>(data: T): Serializable<T> => {
   switch (typeof data) {
@@ -19,36 +23,59 @@ export const serialize = <const T>(data: T): Serializable<T> => {
       }, {} as Serializable<T>);
     }
     case "bigint":
-      return '0x' + data.toString(16) as Serializable<T>;
+      return ("0x" + data.toString(16)) as Serializable<T>;
     default:
       return data as Serializable<T>;
   }
 };
 
-export const deserialize = <T>(data: T): Deserialize<T> => {
+export const deserialize = <
+  const Data extends Serializable<any>,
+  const Dictionary extends DeserializeDictionary<Data> | undefined = undefined,
+>(
+  data: Data,
+  strings?: Dictionary,
+): DeserializeWithDictionary<Data, Dictionary> => {
   switch (typeof data) {
     case "object": {
       if (data === null) {
-        return data as Deserialize<T>;
+        return data as DeserializeWithDictionary<Data, Dictionary>;
       }
 
       if (data instanceof Array) {
-        return data.map(deserialize) as Deserialize<typeof data>;
+        if (data.length === 2) {
+          const stringsData = (strings as [any, any]) || [];
+          return [
+            deserialize(data[0], stringsData[0]),
+            deserialize(data[1], stringsData[1]),
+          ] as DeserializeWithDictionary<Data, Dictionary>;
+        }
+        return data.map((d) =>
+          deserialize(d, strings),
+        ) as DeserializeWithDictionary<Data, Dictionary>;
       }
 
       return Object.entries(data).reduce(
         (deserializedObject, [key, value]) => {
           deserializedObject[key as keyof typeof deserializedObject] =
-            deserialize(value);
+            deserialize(
+              value,
+              (strings || {})[key as keyof typeof strings] as Dictionary,
+            ) as DeserializeWithDictionary<
+              Data,
+              Dictionary
+            >[keyof DeserializeWithDictionary<Data, Dictionary>];
 
           return deserializedObject;
         },
-        {} as Deserialize<typeof data>,
+        {} as DeserializeWithDictionary<Data, Dictionary>,
       );
     }
     case "string":
-      return BigInt(data) as Deserialize<T>;
+      return (
+        strings === true ? data : BigInt(data)
+      ) as DeserializeWithDictionary<Data, Dictionary>;
     default:
-      return data as Deserialize<T>;
+      return data as DeserializeWithDictionary<Data, Dictionary>;
   }
 };
