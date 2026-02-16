8de18f1c7ebabb3aa236b4ac40a758153e38ce20
dumbito <>
2026-02-16 19:48:28 +0000

    feat: add tongo plugin (WIP)

---
+++ packages/tongo/src/index.ts
@@ -0,0 +1,11 @@
+export { TongoPlugin } from './tongo'
+// // Main factory
+// export { PrivacyPoolsV1Protocol } from './plugin/base';
+//
+// // Types
+// export type { SecretManager, SecretManagerParams, ISecretManager } from './account/keys';
+// export type { Commitment, Nullifier } from './account/types';
+// export type { PPv1NetworkConfig as NetworkConfig } from './config';
+//
+// // Configs
+// export { MOCK_POOL_ADDRESS, MOCK_VERIFIER_ADDRESS, SEPOLIA_CONFIG, MAINNET_CONFIG, E_ADDRESS } from './config';
