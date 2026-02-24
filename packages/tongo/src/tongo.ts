a61c41e7484787a535dd2aa75951a508cecebff1
dumbito-notas <tutonoto@fatsolutions.xyz>
2026-02-24 13:15:12 +0000

    chore: lint

--- packages/tongo/src/tongo.ts
+++ packages/tongo/src/tongo.ts
@@ -25,15 +25,18 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
         const client = createPublicClient({ transport: http() })
         //new TongoAccount(0n, "", client); // TODO: typing fails here, but why? viem version?
         const tongoAccount = this.tempmocktongoaccount;//new TongoAccount(0n, "", this.config.client);
+
         return new CustomAccountId(tongoAccount.tongoAddress(), new CustomChainId("tongo-evm", this.config.chain));
     }
 
     async balance(assets: Array<AssetId> | undefined): Promise<Array<AssetAmount>> {
-        let balances = [];
+        const balances = [];
+
         this.config.deploys.forEach(async (tongoAddress, assetId) => {
             if (assets === undefined || assets.includes(assetId)) {
                 const tongoAccount = this.tempmocktongoaccount;//new TongoAccount(0n, tongoAddress, this.config.client);
                 const state = await tongoAccount.state();
+
                 balances.push({ asset: assetId, amount: state.balance + state.pending })
             }
         })
@@ -42,29 +45,40 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
 
     async prepareShield(_asset: AssetAmount, from?: AccountId): Promise<ShieldPreparation> {
         const tongoAddress = this.config.deploys.get(_asset.asset);
+
         if (tongoAddress === undefined) { throw UnsupportedAssetError; }
+
         const tongoAccount = this.tempmocktongoaccount;//new TongoAccount(0n, tongoAddress, this.config.client);
         const fund = await tongoAccount.fund({ amount: _asset.amount, sender: "sender" });
+
         return { txns: [fund.approve, fund.toCalldata()] };
     }
 
     async prepareUnshield(_asset: AssetAmount, to: AccountId): Promise<PrivateOperation> {
         if (_asset.amount > (await this.balance([_asset.asset]))[0]!.amount) { throw InsufficientBalanceError; }
+
         const tongoAddress = this.config.deploys.get(_asset.asset);
+
         if (tongoAddress === undefined) { throw UnsupportedAssetError; }
+
         const tongoAccount = this.tempmocktongoaccount;//new TongoAccount(0n, tongoAddress, this.config.client);
         const rollover = await tongoAccount.rollover({ sender: "sender" });
         const withdraw = await tongoAccount.withdraw({ amount: _asset.amount, to: to.address, sender: "sender" });
+
         return { txns: [rollover.toCalldata(), withdraw.toCalldata()] };
     }
 
     override async prepareTransfer(asset: AssetAmount, to: AccountId): Promise<PrivateOperation> {
         if (asset.amount > (await this.balance([asset.asset]))[0]!.amount) { throw InsufficientBalanceError; }
+
         const tongoAddress = this.config.deploys.get(asset.asset);
+
         if (tongoAddress === undefined) { throw UnsupportedAssetError; }
+
         const tongoAccount = this.tempmocktongoaccount;//new TongoAccount(0n, tongoAddress, this.config.client);
         const rollover = await tongoAccount.rollover({ sender: "sender" });
         const transfer = await tongoAccount.transfer({ amount: asset.amount, to: pubKeyBase58ToAffine(to.address), sender: "sender" });
+
         return { txns: [rollover.toCalldata(), transfer.toCalldata()] };
     }
     override prepareTransferMulti(assets: Array<AssetAmount>, to: AccountId): Promise<PrivateOperation> {
