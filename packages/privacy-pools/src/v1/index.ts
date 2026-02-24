ce7725686c85402c4d164b974ddbda984995a1f0
Gomorongi <elmascapito@fatsolutions.xyz>
2026-02-24 16:36:43 -0300

    Adapt interfaces to new standard

--- packages/privacy-pools/src/v1/index.ts
+++ packages/privacy-pools/src/v1/index.ts
@@ -1,32 +1,37 @@
 import { CreatePluginFn } from "@kohaku-eth/plugins";
 import { PrivacyPoolsBroadcaster, PrivacyPoolsV1Protocol } from "../plugin";
 import { PPv1Instance } from "./instance";
-import { PPv1Plugin, PPv1PluginParameters } from "./interfaces";
+import { PPv1Plugin } from "./interfaces";
 
  
 export const createPPv1Plugin: CreatePluginFn<PPv1Plugin> = (host, params) => {
-    const actualParams = params as PPv1PluginParameters;
-    // setup privacy pools v1 plugin here
+  let instance: PPv1Instance | null = null;
+  const broadcaster = new PrivacyPoolsBroadcaster({
+    host,
+  });
 
-    // ppv1 supports single instance
-    let instance: PPv1Instance | null = null;
-    const broadcaster = new PrivacyPoolsBroadcaster({
-        host,
-    });
-
-    const createInstance = () => {
-        if (instance) {
-            return instance;
-        }
-        instance = new PrivacyPoolsV1Protocol(host, actualParams);
-        return instance;
+  const createInstance = async () => {
+    if (instance) {
+      return instance;
     }
+    const broadcasterUrl = params.broadcasterUrl;
+    const relayersList =
+      typeof broadcasterUrl === "string"
+        ? { default: broadcasterUrl }
+        : broadcasterUrl;
+    instance = new PrivacyPoolsV1Protocol(host, {
+      ...params,
+      relayersList,
+    });
+    await broadcaster.config(params);
+    return instance;
+  };
 
-    return {
-        instances: () => [instance].filter(a => !!a),
-        createInstance,
-        broadcaster,
-        plugin_name: "privacy-pools-v1",
-        params,
-    };
+  return {
+    instances: () => [instance].filter((a) => !!a),
+    createInstance,
+    broadcaster,
+    plugin_name: "privacy-pools-v1",
+    params,
+  };
 };
--- packages/privacy-pools/src/v1/instance.ts
+++ packages/privacy-pools/src/v1/instance.ts
@@ -1,6 +1,6 @@
 import { Address } from 'ox/Address';
-import { AssetAmount, ERC20AssetId, PluginInstance, PublicOperation } from '@kohaku-eth/plugins';
-import { PPv1PrivateOperation } from '../plugin/interfaces/protocol-params.interface';
+import { AssetAmount, ERC20AssetId, PluginInstance } from '@kohaku-eth/plugins';
+import { PPv1PrivateOperation, PPv1PublicOperation } from '../plugin/interfaces/protocol-params.interface';
 
 /**
  * PPv1 uses Ethereum Addresses internally
@@ -8,6 +8,9 @@ import { PPv1PrivateOperation } from '../plugin/interfaces/protocol-params.inter
 export type PPv1Address = Address;
 
 export type PPv1AssetAmount = AssetAmount<ERC20AssetId>;
+export type PPv1AssetBalance = PPv1AssetAmount & {
+    pendingAmount: bigint;
+}
 
 export type PPv1Instance = PluginInstance<
     PPv1Address,
@@ -15,37 +18,12 @@ export type PPv1Instance = PluginInstance<
         input: PPv1AssetAmount,
         internal: PPv1AssetAmount,
         output: PPv1AssetAmount,
-        balance: PPv1AssetAmount
+        balance: PPv1AssetBalance,
     },
-    PublicOperation,
+    PPv1PublicOperation,
     PPv1PrivateOperation,
     {
         prepareShield: true,
-        prepareShieldMulti: true,
         prepareUnshield: true,
-        prepareUnshieldMulti: true,
     }
 >;
-
-export const createInstance = (): PPv1Instance => {
-    const pubKey = "" as Address;
-
-    return {
-        instanceId: () => Promise.resolve(pubKey),
-        balance: () => Promise.resolve([]),
-        prepareShield: () => Promise.resolve({} as PublicOperation),
-        prepareShieldMulti: () => Promise.resolve({} as PublicOperation),
-        prepareUnshield: () => Promise.resolve({} as PPv1PrivateOperation),
-        prepareUnshieldMulti: () => Promise.resolve({} as PPv1PrivateOperation),
-    };
-};
-
-// const x = createInstance();
-
-// x.shield({
-//     asset: {
-//         __type: 'erc20',
-//         contract: '0x0000000000000000000000000000000000000000',
-//     },
-//     amount: 100n,
-// }, '0x1234567890abcdef');
