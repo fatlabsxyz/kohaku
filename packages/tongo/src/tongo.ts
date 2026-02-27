354d8b8a23aae8414876cb1463b5c290f525dc7a
dumbito-notas <tutonoto@fatsolutions.xyz>
2026-02-27 20:49:17 +0000

    clean config

--- packages/tongo/src/tongo.ts
+++ packages/tongo/src/tongo.ts
@@ -1,6 +1,6 @@
 import { Plugin, AssetAmount, ShieldPreparation, PrivateOperation, CustomAccountId, CustomChainId, UnsupportedAssetError, InsufficientBalanceError, Keystore } from "@kohaku-eth/plugins";
 import { AccountId, AssetId, Host, MultiAssetsNotSupportedError } from "@kohaku-eth/plugins";
-import { PublicClient } from "viem";
+import { createPublicClient, http, PublicClient } from "viem";
 import { Address } from "@kohaku-eth/provider";
 import { pubKeyBase58ToAffine, Account as TongoAccount } from "@fatsolutions/tongo-evm";
 
@@ -13,23 +13,28 @@ interface TongoPluginConfig {
 }
 
 export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateOperation> {
-    config: TongoPluginConfig;
+    chain: number;
+    client: PublicClient;
+    deploys: Map<AssetId, Address>;
+    deriveKey: (keystore: Keystore) => bigint;
 
     tempmocktongoaccount!: TongoAccount;
 
-    constructor(readonly host: Host, config: TongoPluginConfig) {
+    constructor(readonly host: Host, {
+        chain = 1,
+        client = createPublicClient({transport: http("localhost:8545") }),
+        deploys = new Map(),
+        deriveKey = TongoPlugin.defaultKeyDerivationBN254
+    }: Partial<TongoPluginConfig> = {}) {
         super();
-        this.config = config;
-
-        if (config.deriveKey === undefined) { this.config.deriveKey = this.defaultKeyDerivationBN254; }
-    }
-
-    private deriveKey(): bigint {
-        return this.config.deriveKey(this.host.keystore);
+        this.chain = chain;
+        this.client = client;
+        this.deploys = deploys;
+        this.deriveKey = deriveKey;
     }
 
     private deriveAccount(tongoContract: string): TongoAccount {
-        return this.deriveAccountFromKey(tongoContract, this.deriveKey());
+        return this.deriveAccountFromKey(tongoContract, this.deriveKey(this.host.keystore));
     }
 
     private deriveAccountFromKey(tongoContract: string, derivedKey: bigint): TongoAccount {
@@ -37,7 +42,7 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
     }
 
     async account(): Promise<AccountId> {
-        return new CustomAccountId(this.deriveAccount("").tongoAddress(), new CustomChainId("tongo-evm", this.config.chain));
+        return new CustomAccountId(this.deriveAccount("").tongoAddress(), new CustomChainId("tongo-evm", this.chain));
     }
 
     private async _balance(account: TongoAccount): Promise<bigint> {
@@ -47,11 +52,11 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
     }
 
     async balance(assets: Array<AssetId> | undefined): Promise<Array<AssetAmount>> {
-        const derivedKey = this.deriveKey();
+        const derivedKey = this.deriveKey(this.host.keystore);
 
         const balances: Promise<AssetAmount>[] = [];
 
-        this.config.deploys.forEach(async (tongoContract, assetId) => {
+        this.deploys.forEach(async (tongoContract, assetId) => {
             if (assets === undefined || assets.includes(assetId)) {
                 const tongoAccount = this.deriveAccountFromKey(tongoContract, derivedKey);
                 const amount = await this._balance(tongoAccount);
@@ -65,7 +70,7 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async prepareShield(asset: AssetAmount, from?: AccountId): Promise<ShieldPreparation> {
-        const tongoContract = this.config.deploys.get(asset.asset);
+        const tongoContract = this.deploys.get(asset.asset);
 
         if (tongoContract === undefined) { throw UnsupportedAssetError; }
 
@@ -75,7 +80,7 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
     }
 
     async prepareUnshield(asset: AssetAmount, to: AccountId): Promise<PrivateOperation> {
-        const tongoContract = this.config.deploys.get(asset.asset);
+        const tongoContract = this.deploys.get(asset.asset);
 
         if (tongoContract === undefined) { throw UnsupportedAssetError; }
 
@@ -90,7 +95,7 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
     }
 
     override async prepareTransfer(asset: AssetAmount, to: AccountId): Promise<PrivateOperation> {
-        const tongoContract = this.config.deploys.get(asset.asset);
+        const tongoContract = this.deploys.get(asset.asset);
 
         if (tongoContract === undefined) { throw UnsupportedAssetError; }
 
@@ -113,9 +118,9 @@ export class TongoPlugin extends Plugin<AssetAmount, ShieldPreparation, PrivateO
         throw new Error("Method not implemented.");
     }
 
-    defaultKeyDerivationBN254() {
+    static defaultKeyDerivationBN254(keystore: Keystore) {
         const accountIndex = "0";
-        const derivation = BigInt(this.host.keystore.deriveAt("m/701160/"+accountIndex)); //TONGO
+        const derivation = BigInt(keystore.deriveAt("m/701160/"+accountIndex)); //TONGO
         const BN254_GROUP_ORDER = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001n;
 
         return derivation % BN254_GROUP_ORDER;
