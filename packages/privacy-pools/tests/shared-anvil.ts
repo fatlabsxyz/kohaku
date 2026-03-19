6a7f3726550c87be8932d4622ca8041635cadfb8
Gomorongi <elmascapito@fatsolutions.xyz>
2026-03-19 17:56:35 -0300

    Disable shared anvil instance

--- packages/privacy-pools/tests/shared-anvil.ts
+++ packages/privacy-pools/tests/shared-anvil.ts
@@ -1,26 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { chainConfigSetup } from "./constants";
-import { AnvilInstance, defineAnvil } from "./utils/anvil";
+import {
+  AnvilInstance,
+  // defineAnvil 
+} from "./utils/anvil";
 import type { TestProject } from "vitest/node";
 
 const chainId = 11155111;
 const {
     rpcUrl,
-    forkBlockNumber,
+    // forkBlockNumber,
 } = chainConfigSetup[chainId];
 
 export async function setup(project: TestProject) {
-    if (!(globalThis as any).anvilInstance) {
-        const anvilInstance = await defineAnvil({forkUrl: rpcUrl, chainId, forkBlockNumber});
+  if (!(globalThis as any).anvilInstance) {
+      // const anvilInstance = await defineAnvil({forkUrl: rpcUrl, chainId, forkBlockNumber});
 
-        (globalThis as any).anvilInstance = anvilInstance;
-        await anvilInstance.start();
-        project.provide('rpcUrl', `http://127.0.0.1:${anvilInstance.port}/1`);
-    }
+      // (globalThis as any).anvilInstance = anvilInstance;
+      // await anvilInstance.start();
+      project.provide('rpcUrl', rpcUrl);
+  }
 }
 
 export async function teardown() {
-    const anvilInstance: AnvilInstance | undefined = (globalThis as any).anvilInstance;
+  const anvilInstance: AnvilInstance | undefined = (globalThis as any).anvilInstance;
 
   if (anvilInstance) {
     (globalThis as any).anvilInstance.stop();
